77fe44916cfbcfee713a5ca3976870c0f292a05d
Ivan Kiiashko <89341731+MrQuinlan@users.noreply.github.com>
2022-02-01 14:09:12 +0200

    1st-tsk

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+    bodyRef: document.querySelector('body'),
+    startBtnRef: document.querySelector('button[data-start]'),
+    stopBtnRef: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtnRef.addEventListener('click', () => {
+    refs.startBtnRef.setAttribute('disabled', '');
+    intervalId = setInterval(() => {
+        refs.bodyRef.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+refs.stopBtnRef.addEventListener('click', () => {
+    refs.startBtnRef.removeAttribute('disabled');
+    clearInterval(intervalId);
+});
